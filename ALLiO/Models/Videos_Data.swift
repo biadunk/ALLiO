417cb14538fa08d64f4cd99d4f1c3f2351333686
Kacper Biaduń <kacperbiadun.12@gmail.com>
2023-09-19 23:47:10 +0200

    test commit

---
+++ ALLiO/Models/Videos_Data.swift
@@ -0,0 +1,8 @@
+//
+//  Videos_Data.swift
+//  ALLiO
+//
+//  Created by Kacper Biaduń on 06/09/2023.
+//
+
+import Foundation
---
+++ ALLiO/Views/ALLiO_News_Comment_View.swift
@@ -0,0 +1,20 @@
+//
+//  ALLiO_News_Comment_View.swift
+//  ALLiO
+//
+//  Created by Kacper Biaduń on 18/09/2023.
+//
+
+import SwiftUI
+
+struct ALLiO_News_Comment_View: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ALLiO_News_Comment_View_Previews: PreviewProvider {
+    static var previews: some View {
+        ALLiO_News_Comment_View()
+    }
+}
